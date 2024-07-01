80413e1663a693d05275744179f0d61bb083025e
raczmirko <m1rk0d3v@gmail.com>
2024-07-01 16:44:54 +0200

    feat: added WelcomeCard

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,14 @@
 import Container from "@mui/material/Container";
 import ParticleBackground from "./ParticleBackground";
-import PageHeader from "./PageHeader";
-import Colors from "./colorUtil";
+import WelcomeCard from "./WelcomeCard";
 
 const Home = () => {
     return(
-        <Container component="main" sx={{ marginTop:'20px' , padding:'10px', justifyContent: 'center'}}>
+        <Container component="main" sx={{ marginTop:'20px' , padding:'10px', justifyContent: 'center' }}>
             <Container sx={{position: 'absolute', zIndex:-1}}>
                 <ParticleBackground />
             </Container>
-            <PageHeader text='Home - What is this site about?' bgColor={Colors.PINE}/>
+            <WelcomeCard />
         </Container>
     )
 }
