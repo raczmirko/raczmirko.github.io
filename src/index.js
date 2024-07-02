2edf40c2695a1804f9c9f234951224f0098f7b10
raczmirko <m1rk0d3v@gmail.com>
2024-07-02 23:01:56 +0200

    feat: added i18n translation dependency and JSON locales

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { I18nextProvider } from 'react-i18next';
 import { HashRouter } from 'react-router-dom';
 import App from './app/App';
 import { NotificationProvider } from './app/NotificationProvider';
+import i18n from './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <NotificationProvider>
       <HashRouter>
-        <App />
+        <I18nextProvider i18n={i18n}>
+          <App />
+        </I18nextProvider>
       </HashRouter>
     </NotificationProvider>
   </React.StrictMode>
