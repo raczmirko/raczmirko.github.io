4d36b93074d89ae3dac2217ad95c4d3f6eade034
raczmirko <m1rk0d3v@gmail.com>
2024-07-03 14:52:44 +0200

    feat: added LineDivider and BlockTitle components

---
+++ src/components/LineDivider.jsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+
+const LineDivider = () => {
+    return(
+        <Box
+            sx={{
+            height: '2px',
+            backgroundColor: 'white',
+            width: '100%',
+            marginBottom: '8px'
+            }}
+        />
+    )
+}
+
+export default LineDivider;
