350f07e1ba98a47b71562286790dfde6269e2176
raczmirko <m1rk0d3v@gmail.com>
2024-07-06 17:43:03 +0200

    feat: added complementary colours

--- src/utils/colorUtil.js
+++ src/utils/colorUtil.js
@@ -1,7 +1,34 @@
 const Colors = {
+    // Complementary colours are divided by comments
     PURPLE: '#4c044c',
     PINE: '#394827',
-    GREEN: '#759450'
+    //----------//
+    TEAL: '#008080',
+    CORAL: '#ff7f50',
+    //----------//
+    NAVY: '#000080',
+    GOLD: '#ffd700',
+    //----------//
+    TURQUOISE: '#40e0d0',
+    MAROON: '#800000',
+    //----------//
+    INDIGO: '#4b0082',
+    AMBER: '#ffbf00',
+    //----------//
+    CYAN: '#00ffff',
+    VERMILLION: '#ff4500',
+    //----------//
+    PINK: '#ffc0cb',
+    LIME: '#00ff00',
+    //----------//
+    CRIMSON: '#dc143c',
+    MINT: '#98ff98',
+    //----------//
+    EMERALD: '#50c878',
+    PEACH: '#ffdab9',
+    //----------//
+    GREEN: '#759450',
+    WHITE: '#FFFFFF'
 };
 
 export default Colors;
