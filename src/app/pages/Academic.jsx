b72d187363e6c5b3e4c3bbc336976544fdcd3cc3
raczmirko <m1rk0d3v@gmail.com>
2024-07-06 17:43:54 +0200

    feat: added Academic page skeleton

---
+++ src/app/pages/Academic.jsx
@@ -0,0 +1,57 @@
+import { Box, Grid } from '@mui/material';
+import Container from "@mui/material/Container";
+import { useTranslation } from 'react-i18next';
+import BlockTitle from '../../components/BlockTitle';
+import CustomAccordion from '../../components/CustomAccordion';
+import JustifiedTextCard from '../../components/JustifiedTextCard';
+import ParticleBackground from "../../components/ParticleBackground";
+import TitleCard from '../../components/TitleCard';
+import Colors from '../../utils/colorUtil';
+import PresentationCards from '../../components/PresentationCards';
+
+const Academic = () => {
+    const { t: translate } = useTranslation();
+    return(
+        <Container component="main" sx={{ marginTop:'20px' , padding:'10px', justifyContent: 'center' }}>
+            <Container sx={{position: 'absolute', zIndex:-1}}>
+                <ParticleBackground />
+            </Container>
+            <TitleCard  title={translate('academic.title')} 
+                        bgColor={Colors.AMBER}
+                        textColor={'black'}/>
+            <BlockTitle title={translate('general.summary')}/>
+            <Box sx={{ marginTop: 2 }} />
+            <Grid container spacing={{ xs: 1, sm: 2 }} justifyContent="center">
+                <Grid item xs={12} sm={12}>
+                    <JustifiedTextCard text={translate('academic.summary')}/>
+                </Grid>
+            </Grid>
+            <Grid container spacing={{ xs: 1, sm: 2 }} justifyContent="center">
+                <Grid item xs={12} sm={12}>
+                    <CustomAccordion 
+                        title={translate('academic.presentations.title')} 
+                        body={PresentationCards}
+                        bgColor={Colors.CRIMSON}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={12}>
+                    <CustomAccordion 
+                        title={translate('academic.publications.title')} 
+                        body={PresentationCards}
+                        bgColor={Colors.WHITE}
+                        textColor={'black'}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={12}>
+                    <CustomAccordion 
+                        title={translate('academic.achievements.title')} 
+                        body={PresentationCards}
+                        bgColor={Colors.GREEN}
+                    />
+                </Grid>
+            </Grid>
+        </Container>
+    )
+}
+
+export default Academic;
