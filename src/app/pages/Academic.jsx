ff4dbf5d06645237b2f049679c52501ef209a367
raczmirko <m1rk0d3v@gmail.com>
2024-07-07 11:41:09 +0200

    feat: added PublicationCards

--- src/app/pages/Academic.jsx
+++ src/app/pages/Academic.jsx
@@ -8,6 +8,7 @@ import ParticleBackground from "../../components/ParticleBackground";
 import TitleCard from '../../components/TitleCard';
 import Colors from '../../utils/colorUtil';
 import PresentationCards from '../../components/PresentationCards';
+import PublicationCards from '../../components/PublicationCards';
 
 const Academic = () => {
     const { t: translate } = useTranslation();
@@ -37,7 +38,7 @@ const Academic = () => {
                 <Grid item xs={12} sm={12}>
                     <CustomAccordion 
                         title={translate('academic.publications.title')} 
-                        body={PresentationCards}
+                        body={PublicationCards}
                         bgColor={Colors.WHITE}
                         textColor={'black'}
                     />
