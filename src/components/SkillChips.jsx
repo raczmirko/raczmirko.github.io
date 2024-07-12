c1af7a6e4500da9f41b25bc671aeb3c6f72d1368
raczmirko <m1rk0d3v@gmail.com>
2024-07-12 18:57:12 +0200

    feat: added dynamically generated skill chips

---
+++ src/components/SkillChips.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Box, Chip } from '@mui/material';
+import * as Icons from '@mui/icons-material';
+import skillsData from '../assets/skills.json';
+import { paragraphSize } from '../assets/styles';
+
+const SkillChips = () => {
+  return (
+    <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1 }}>
+      {skillsData.skills.map((skill, index) => {
+        const IconComponent = Icons[skill.icon];
+        return (
+          <Chip
+            key={index}
+            icon={IconComponent ? <IconComponent fontSize="medium"/> : null}
+            label={skill.name}
+            size='string'
+            variant="outlined"
+            sx={{fontSize: paragraphSize}}
+          />
+        );
+      })}
+    </Box>
+  );
+}
+
+export default SkillChips;
