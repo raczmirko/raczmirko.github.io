bfcaa1eff2452cc5a91c089bcbaffafb68654054
raczmirko <123543210+raczmirko@users.noreply.github.com>
2024-07-20 18:05:23 +0200

    feat: added projects

--- src/utils/imageMap.js
+++ src/utils/imageMap.js
@@ -9,11 +9,39 @@ import animalcam_3 from '../assets/img/animalcam/animalcam_3.jpg';
 import animalcam_4 from '../assets/img/animalcam/animalcam_4.jpg';
 import animalcam_5 from '../assets/img/animalcam/animalcam_5.jpg';
 import animalcam_6 from '../assets/img/animalcam/animalcam_6.jpg';
-
+import personal_project_manager_1 from '../assets/img/personal_project_manager/personal_project_manager_1.png';
+import personal_project_manager_2 from '../assets/img/personal_project_manager/personal_project_manager_2.png';
+import personal_project_manager_3 from '../assets/img/personal_project_manager/personal_project_manager_3.png';
+import personal_project_manager_4 from '../assets/img/personal_project_manager/personal_project_manager_4.png';
+import puzzle_center_1 from '../assets/img/puzzle_center/puzzle_center_1.png';
+import puzzle_center_2 from '../assets/img/puzzle_center/puzzle_center_2.png';
+import puzzle_center_3 from '../assets/img/puzzle_center/puzzle_center_3.png';
+import puzzle_center_4 from '../assets/img/puzzle_center/puzzle_center_4.png';
+import puzzle_center_5 from '../assets/img/puzzle_center/puzzle_center_5.png';
+import puzzle_center_6 from '../assets/img/puzzle_center/puzzle_center_6.png';
+import puzzle_center_7 from '../assets/img/puzzle_center/puzzle_center_7.png';
+import truck_timer_1 from '../assets/img/truck_timer/truck_timer_1.png';
+import truck_timer_2 from '../assets/img/truck_timer/truck_timer_2.png';
+import truck_timer_3 from '../assets/img/truck_timer/truck_timer_3.png';
+import truck_timer_4 from '../assets/img/truck_timer/truck_timer_4.png';
+import truck_timer_5 from '../assets/img/truck_timer/truck_timer_5.png';
+import product_rating_app_1 from '../assets/img/product_rating_app/product_rating_app_1.png';
+import product_rating_app_2 from '../assets/img/product_rating_app/product_rating_app_2.png';
+import product_review_app_1 from '../assets/img/product_review_app/product_review_app_1.png';
+import product_review_app_2 from '../assets/img/product_review_app/product_review_app_2.png';
+import product_review_app_3 from '../assets/img/product_review_app/product_review_app_3.png';
+import product_review_app_4 from '../assets/img/product_review_app/product_review_app_4.png';
+import product_review_app_5 from '../assets/img/product_review_app/product_review_app_5.png';
+import product_review_app_6 from '../assets/img/product_review_app/product_review_app_6.png';
 
 export const images = {
-  birdcam: [birdcam_1, birdcam_2],
-  log_change_email_notifier: [log_change_notifier_1],
-  birdcam_frame: [birdcam_frame_1, birdcam_frame_2],
-  animalcam: [animalcam_1, animalcam_2, animalcam_3, animalcam_4, animalcam_5, animalcam_6]
+    birdcam: [birdcam_1, birdcam_2],
+    log_change_email_notifier: [log_change_notifier_1],
+    birdcam_frame: [birdcam_frame_1, birdcam_frame_2],
+    animalcam: [animalcam_1, animalcam_2, animalcam_3, animalcam_4, animalcam_5, animalcam_6],
+    personal_project_manager: [personal_project_manager_1, personal_project_manager_2, personal_project_manager_3, personal_project_manager_4],
+    puzzle_center: [puzzle_center_1, puzzle_center_2, puzzle_center_3, puzzle_center_4, puzzle_center_5, puzzle_center_6, puzzle_center_7],
+    truck_timer: [truck_timer_1, truck_timer_2, truck_timer_3, truck_timer_4, truck_timer_5],
+    product_rating_app: [product_rating_app_1, product_rating_app_2],
+    product_review_app: [product_review_app_1, product_review_app_2, product_review_app_3, product_review_app_4, product_review_app_5, product_review_app_6]
 };
