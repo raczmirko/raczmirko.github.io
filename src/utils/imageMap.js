b4bc9db00b50764670dff062093497a81382d0d0
Mirko Racz <123543210+raczmirko@users.noreply.github.com>
2024-07-20 13:22:57 +0200

    feat: added project details, removed mock projects

--- src/utils/imageMap.js
+++ src/utils/imageMap.js
@@ -1,6 +1,8 @@
 import birdcam_1 from '../assets/img/birdcam/birdcam_1.jpg';
 import birdcam_2 from '../assets/img/birdcam/birdcam_2.jpg';
+import log_change_notifier_1 from '../assets/img/log_change_email_notifier/log_change_notifier_1.png';
 
 export const images = {
   birdcam: [birdcam_1, birdcam_2],
+  log_change_email_notifier: [log_change_notifier_1]
 };
