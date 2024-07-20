4eb076db07060cb8842ef090c7fae507aa26d36b
Mirko Racz <123543210+raczmirko@users.noreply.github.com>
2024-07-20 14:55:17 +0200

    feat: added AnimalCam project

--- src/utils/imageMap.js
+++ src/utils/imageMap.js
@@ -3,9 +3,17 @@ import birdcam_2 from '../assets/img/birdcam/birdcam_2.jpg';
 import log_change_notifier_1 from '../assets/img/log_change_email_notifier/log_change_notifier_1.png';
 import birdcam_frame_1 from '../assets/img/birdcam_frame/birdcam_frame_1.jpg';
 import birdcam_frame_2 from '../assets/img/birdcam_frame/birdcam_frame_2.jpg';
+import animalcam_1 from '../assets/img/animalcam/animalcam_1.jpg';
+import animalcam_2 from '../assets/img/animalcam/animalcam_2.jpg';
+import animalcam_3 from '../assets/img/animalcam/animalcam_3.jpg';
+import animalcam_4 from '../assets/img/animalcam/animalcam_4.jpg';
+import animalcam_5 from '../assets/img/animalcam/animalcam_5.jpg';
+import animalcam_6 from '../assets/img/animalcam/animalcam_6.jpg';
+
 
 export const images = {
   birdcam: [birdcam_1, birdcam_2],
   log_change_email_notifier: [log_change_notifier_1],
-  birdcam_frame: [birdcam_frame_1, birdcam_frame_2]
+  birdcam_frame: [birdcam_frame_1, birdcam_frame_2],
+  animalcam: [animalcam_1, animalcam_2, animalcam_3, animalcam_4, animalcam_5, animalcam_6]
 };
