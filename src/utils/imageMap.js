2c29c53b0fe002c7d8f47a3e91552ecea215445d
Mirko Racz <123543210+raczmirko@users.noreply.github.com>
2024-07-20 14:37:49 +0200

    feat: added Birdcam frame project

--- src/utils/imageMap.js
+++ src/utils/imageMap.js
@@ -1,8 +1,11 @@
 import birdcam_1 from '../assets/img/birdcam/birdcam_1.jpg';
 import birdcam_2 from '../assets/img/birdcam/birdcam_2.jpg';
 import log_change_notifier_1 from '../assets/img/log_change_email_notifier/log_change_notifier_1.png';
+import birdcam_frame_1 from '../assets/img/birdcam_frame/birdcam_frame_1.jpg';
+import birdcam_frame_2 from '../assets/img/birdcam_frame/birdcam_frame_2.jpg';
 
 export const images = {
   birdcam: [birdcam_1, birdcam_2],
-  log_change_email_notifier: [log_change_notifier_1]
+  log_change_email_notifier: [log_change_notifier_1],
+  birdcam_frame: [birdcam_frame_1, birdcam_frame_2]
 };
