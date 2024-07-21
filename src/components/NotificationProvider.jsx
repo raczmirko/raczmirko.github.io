2b42a678b821072a426f12f4317e64e7787acffb
raczmirko <123543210+raczmirko@users.noreply.github.com>
2024-07-21 08:42:16 +0200

    refactor: moved components to components package

--- src/components/NotificationProvider.jsx
+++ src/components/NotificationProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState } from 'react';
-import Notification from '../components/Notification';
+import Notification from './Notification';
 
 const NotificationContext = createContext();
 
